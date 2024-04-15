69d4ce2f1604fd06e4c34362706ba9d496f501c5
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2024-04-15 22:08:24 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #=================================================
-# DaoDao's script
+# JayKwok's script
 #=================================================
 ##添加自己的插件库
 rm -rf ./package/lean/k3screenctrl
 rm -rf ./package/lean/autocore
                
-sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
+sed -i "1isrc-git extraipk https://github.com/zijieKwok/jaykwok-ipk\n" feeds.conf.default
 
