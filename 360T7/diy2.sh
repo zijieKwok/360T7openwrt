50a99c9923386698856c71757180867dbdd296b7
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2024-04-16 16:04:54 +0800

    Update diy2.sh

--- 360T7/diy2.sh
+++ 360T7/diy2.sh
@@ -14,6 +14,7 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='JayKwok $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
+cp -af feeds/extraipk/patch/diy/banner  package/base-files/files/etc/banner
 echo "DISTRIB_MODEL='360T7'" >> package/base-files/files/etc/openwrt_release
 target=$(grep -m 1 "CONFIG_TARGET_.*_.*=y" .config | sed "s/CONFIG_TARGET_\(.*\)_\(.*\)=y/\1\/\2/g")
 for configFile in $(ls target/linux/$target/config*)
