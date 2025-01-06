53651ef05b9867c2098fabe8318f88e12e033f17
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-06 18:36:27 +0800

    Update diy1.sh

--- 360T7/diy1.sh
+++ 360T7/diy1.sh
@@ -13,4 +13,5 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo -e "\nsrc-git extraipk https://github.com/zijieKwok/jaykwok-ipk" >> feeds.conf.default
+echo -e "\nsrc-git openwrt-24.10 https://github.com/openwrt/packages.git;openwrt-24.10" >> feeds.conf.default
 # Add feed sources
