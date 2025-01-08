999602d13580d9b72eec92b9f3d34df14e489ad6
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-08 17:59:32 +0800

    Update diy2.sh

--- 360T7/diy2.sh
+++ 360T7/diy2.sh
@@ -16,13 +16,13 @@ sed -i "s/hostname='ImmortalWrt'/hostname='OpenWrt'/g" package/base-files/files/
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/base-files/files/etc/openwrt_release
 cp -af feeds/extraipk/patch/diy/banner  package/base-files/files/etc/banner
-sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+# sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 ##New WiFi
 sed -i "s/ImmortalWrt-2.4G/OpenWrt_2.4G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i "s/ImmortalWrt-5G/OpenWrt_5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 ##更新FQ
-rm -rf feeds/packages/net/tailscale/*
-cp -af feeds/extraipk/tailscale/*  feeds/packages/net/tailscale/
+# rm -rf feeds/packages/net/tailscale/*
+# cp -af feeds/extraipk/tailscale/*  feeds/packages/net/tailscale/
 
 ##MosDNS
 rm -rf feeds/packages/net/mosdns/*
