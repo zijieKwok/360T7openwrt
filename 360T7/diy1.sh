01dcc1433181430a7d90b397e565581e89d7f202
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-18 16:49:16 +0800

    Update diy1.sh

--- 360T7/diy1.sh
+++ 360T7/diy1.sh
@@ -13,3 +13,6 @@
 # Uncomment a feed source
 echo -e "\nsrc-git extraipk https://github.com/zijieKwok/jaykwok-ipk" >> feeds.conf.default
 # Add feed sources
+echo 'src-git store https://github.com/linkease/istore;main' >>feeds.conf.default
+echo 'src-git linkease_nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
+echo 'src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
