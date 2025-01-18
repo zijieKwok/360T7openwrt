f4a3d2dda10073fa252d30b162303996beb32116
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-01-18 23:26:39 +0800

    Update diy2.sh

--- 360T7/diy2.sh
+++ 360T7/diy2.sh
@@ -28,10 +28,10 @@ rm -rf feeds/packages/net/tailscale/*
 cp -af feeds/extraipk/tailscale/*  feeds/packages/net/tailscale/
 
 ##MosDNS
-rm -rf feeds/packages/net/mosdns/*
-cp -af feeds/extraipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
-rm -rf feeds/packages/net/v2ray-geodata/*
-cp -af feeds/extraipk/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
+# rm -rf feeds/packages/net/mosdns/*
+# cp -af feeds/extraipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
+# rm -rf feeds/packages/net/v2ray-geodata/*
+# cp -af feeds/extraipk/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
 
 rm -rf feeds/luci/applications/luci-app-openclash/*
 cp -af feeds/extraipk/patch/wall-luci/luci-app-openclash/*  feeds/luci/applications/luci-app-openclash/
