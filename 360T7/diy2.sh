3749e0f383717d9c06e6a2e24bd72f3ee109c8d1
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-02-11 18:22:21 +0800

    Update diy2.sh

--- 360T7/diy2.sh
+++ 360T7/diy2.sh
@@ -31,8 +31,8 @@ sed -i "s/MT7981_AX3000_2.4G/OpenWrt-2.4G/g" package/mtk/drivers/wifi-profile/fi
 sed -i "s/MT7981_AX3000_5G/OpenWrt-5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
 ##更新tailscale
 rm -rf feeds/packages/net/tailscale
-# cp -af feeds/extraipk/tailscale/tailscale  feeds/packages/net/
-# sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+cp -af feeds/extraipk/tailscale/tailscale  feeds/packages/net/
+sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 rm -rf package/feeds/extraipk/op-daed
 
 ##MosDNS
