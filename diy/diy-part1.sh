15a113ceb294de61d85d69aec5c06e19a9fd8524
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-02-12 17:38:34 +0800

    Update diy-part1.sh

--- diy/diy-part1.sh
+++ diy/diy-part1.sh
@@ -14,9 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo -e "\nsrc-git extraipk https://github.com/Jaykwok2999/istoreos-ipk" >> feeds.conf.default
+echo 'src-git store https://github.com/linkease/istore;main' >>feeds.conf.default
 
 
