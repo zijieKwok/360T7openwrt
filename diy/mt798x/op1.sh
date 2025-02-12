999aa1e22b95e0e4e3634ec442ea4b4b931a459c
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-02-12 21:30:35 +0800

    Delete diy directory

--- diy/mt798x/op1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#=================================================
-# JayKwok's script
-#=================================================
-##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/zijieKwok/jaykwok-ipk" >> feeds.conf.default
