cbc19cbcab8228a54155322b0497e56d706ea613
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-02-12 21:53:50 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -6,5 +6,5 @@
 rm -rf ./package/lean/k3screenctrl
 rm -rf ./package/lean/autocore
                
-sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
+sed -i "1isrc-git extraipk https://github.com/Jaykwok2999/istoreos-ipk\n" feeds.conf.default
 
