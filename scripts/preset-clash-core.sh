9133ac48528ebc3f0aa749e43539cc26fe99c0cd
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-02-14 20:35:21 +0800

    Create preset-clash-core.sh

---
+++ scripts/preset-clash-core.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p files/etc/openclash/core
+
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/meta/clash-linux-${1}.tar.gz"
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+
+chmod +x files/etc/openclash/core/clash*
