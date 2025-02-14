c976b7382dd82653324f35f0fe0f361392686608
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-02-14 20:48:04 +0800

    Update lean01.sh

--- diy/lean/lean01.sh
+++ diy/lean/lean01.sh
@@ -8,5 +8,5 @@ rm -rf ./package/lean/autocore
                
 sed -i "1isrc-git istoreos_ipk https://github.com/Jaykwok2999/istoreos-ipk\n" feeds.conf.default
 
-sed -i 's/#src-git luci/src-git luci/g' feeds.conf.default
-sed -i '/luci.git;openwrt-23.05/d' feeds.conf.default
+# sed -i 's/#src-git luci/src-git luci/g' feeds.conf.default
+# sed -i '/luci.git;openwrt-23.05/d' feeds.conf.default
