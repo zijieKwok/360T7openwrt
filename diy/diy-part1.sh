06980e0d98ad2d394f6fa48988270bc1155ae0f5
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-03-06 22:57:30 +0800

    Update diy-part1.sh

--- diy/diy-part1.sh
+++ diy/diy-part1.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#=================================================
+# JayKwok's script
+#=================================================
+##添加自己的插件库
+rm -rf ./package/lean/k3screenctrl
+rm -rf ./package/lean/autocore
+               
+sed -i "1isrc-git istoreos_ipk https://github.com/Jaykwok2999/istoreos-ipk\n" feeds.conf.default
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git istoreos_ipk https://github.com/Jaykwok2999/istoreos-ipk' >>feeds.conf.default
-echo 'src-git store https://github.com/linkease/istore' >>feeds.conf.default
-
+# sed -i 's/#src-git luci/src-git luci/g' feeds.conf.default
+# sed -i '/luci.git;openwrt-23.05/d' feeds.conf.default
