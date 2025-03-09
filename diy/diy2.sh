0787437b8fb30afd470dc155d8f735e2db4bae52
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2025-03-09 18:35:19 +0800

    Update diy2.sh

--- diy/diy2.sh
+++ diy/diy2.sh
@@ -14,26 +14,19 @@ echo -e "msgstr \"网络存储\"" >> feeds/luci/modules/luci-base/po/zh-cn/base.
 echo -e "\nmsgid \"VPN\"" >> feeds/luci/modules/luci-base/po/zh-cn/base.po
 echo -e "msgstr \"魔法网络\"" >> feeds/luci/modules/luci-base/po/zh-cn/base.po
 
-##补充汉化
-echo -e "\nmsgid \"General\"" >> package/feeds/luci/luci-app-dnsforwarder/po/zh-cn/dnsforwarder.po
-echo -e "msgstr \"常规\"" >> package/feeds/luci/luci-app-dnsforwarder/po/zh-cn/dnsforwarder.po
-
-echo -e "\nmsgid \"LOG\"" >> package/feeds/luci/luci-app-dnsforwarder/po/zh-cn/dnsforwarder.po
-echo -e "msgstr \"日志\"" >> package/feeds/luci/luci-app-dnsforwarder/po/zh-cn/dnsforwarder.po
 
 ##配置ip等
-sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3| ; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
 sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
 
 ##WiFi
-sed -i "s/LEDE/OpenWrt/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i "s/LEDE/OpenWrt/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 ##替换K3无线驱动为69027
 # rm -rf ./package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 # cp -af feeds/istoreos_ipk/patch/brcmfmac4366c-pcie.bin ./package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 
 ##取消bootstrap为默认主题
-rm -rf ./feeds/luci/themes/luci-theme-design
+# rm -rf ./feeds/luci/themes/luci-theme-design
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
@@ -43,15 +36,12 @@ sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt-$(date +%Y%m%d)
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By JayKwok'/g" package/lean/default-settings/files/zzz-default-settings
 cp -af feeds/istoreos_ipk/patch/diy/banner  package/base-files/files/etc/banner
 
-sed -i "2iuci set istore.istore.channel='OpenWrt_JayKwok'" package/lean/default-settings/files/zzz-default-settings
-sed -i "3iuci commit istore" package/lean/default-settings/files/zzz-default-settings
+# sed -i "2iuci set istore.istore.channel='OpenWrt_JayKwok'" package/lean/default-settings/files/zzz-default-settings
+# sed -i "3iuci commit istore" package/lean/default-settings/files/zzz-default-settings
 
-# rm -rf feeds/packages/lean/default-settings/files/zzz-default-settings
-# cp -af feeds/istoreos_ipk/patch/diy/zzz-default-settings feeds/packages/lean/default-settings/files/
 rm -rf feeds/packages/net/xray-core
 cp -af feeds/istoreos_ipk/xray-core feeds/packages/net/
 # rm -rf feeds/istoreos_ipk/xray-core
-# rm -rf feeds/istoreos_ipk/theme/luci-theme-argon
 
 # tailscale
 # rm -rf feeds/packages/net/tailscale
@@ -79,9 +69,9 @@ sed -i '/TARGET_LDFLAGS += -lubox -lubus/i\TARGET_LDFLAGS += -Wl,--gc-sections -
 cp -af feeds/istoreos_ipk/patch/rockchip/*  target/linux/rockchip/armv8/base-files/
 
 # golong1.23依赖
-rm -rf feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
 # git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-git clone https://git.kejizero.online/zhao/packages_lang_golang -b 23.x feeds/packages/lang/golang
+# git clone https://git.kejizero.online/zhao/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 #修复TailScale配置文件冲突
 TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
